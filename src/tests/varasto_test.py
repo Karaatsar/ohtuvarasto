ee4c408624ac2642222d08b055406229d03dc113
Karaatsar <kara.atsar@helsinki.fi>
2024-11-04 14:52:37 +0200

    varastojuttuja

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,51 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_negatiivinen_tilavuus(self):
+        varasto = Varasto(-10)
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+
+    def test_negatiivinen_alku_saldo(self):
+        varasto = Varasto(10, -5)
+        self.assertAlmostEqual(varasto.saldo, 0)
+
+    def test_alku_saldo_suurempi_kuin_tilavuus(self):
+        varasto = Varasto(10, 15)
+        self.assertAlmostEqual(varasto.saldo, 10)
+
+    def test_negatiivinen_maara_lisattaessa(self):
+        alkuperainen_saldo = self.varasto.saldo
+        self.varasto.lisaa_varastoon(-5)
+        self.assertAlmostEqual(self.varasto.saldo, alkuperainen_saldo)
+
+    def test_lisataan_enemman_kuin_mahtuu(self):
+        self.varasto.lisaa_varastoon(15)
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_negatiivinen_maara_otettaessa(self):
+        alkuperainen_saldo = self.varasto.saldo
+        saatu_maara = self.varasto.ota_varastosta(-5)
+        self.assertAlmostEqual(saatu_maara, 0)
+        self.assertAlmostEqual(self.varasto.saldo, alkuperainen_saldo)
+
+    def test_otetaan_enemman_kuin_on(self):
+        self.varasto.lisaa_varastoon(5)
+        saatu_maara = self.varasto.ota_varastosta(10)
+        self.assertAlmostEqual(saatu_maara, 5)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_str_palauttaa_oikean_merkkijonon(self): 
+       # Aluksi saldo on 0 ja tilaa on tilavuus
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
+
+       # Lisätään saldoa ja tarkistetaan tulos uudelleen
+        self.varasto.lisaa_varastoon(5)
+        self.assertEqual(str(self.varasto), "saldo = 5, vielä tilaa 5")
+
+       # Otetaan saldoa ja tarkistetaan tulos uudelleen
+        self.varasto.ota_varastosta(3)
+        self.assertEqual(str(self.varasto), "saldo = 2, vielä tilaa 8")
+
+if __name__=="__main__":
+   unittest.main()
